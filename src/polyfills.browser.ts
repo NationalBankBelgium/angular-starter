528dbe3894dbd8010ed7b4ff5f359386b7c5a033
christophercr <christophercr@gmail.com>
2018-01-29 14:14:39 +0100

    Add polyfills for IE11 (otherwise IE will throw!!)

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -22,27 +22,12 @@
 // import 'ie-shim';
 
 // IE9, IE10 and IE11 requires all of the following polyfills.
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
+import 'core-js/es6';
+import 'core-js/es7/reflect';
+import 'core-js/stage/4';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-// Evergreen browsers require these.
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
 
 /**
  * Required to support Web Animations `@angular/animation`.
@@ -54,6 +39,8 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
+// workaround for IE11 before loading zone.ks (see: https://github.com/angular/zone.js/issues/933)
+(window as any).__Zone_enable_cross_context_check = true;
 import 'zone.js/dist/zone';
 // import 'zone.js/dist/long-stack-trace-zone' // async stack traces with zone.js included for dev
 
